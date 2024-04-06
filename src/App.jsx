96fda9125a5e0f83b112c18e8e9273b484314d56
Janella <fullmoonfirefly11@gmail.com>
2024-04-06 01:10:12 -0400

    Add routing and base for each page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import './App.css'
 import { createTheme, styled, ThemeProvider } from '@mui/material/styles';
 import { AppBar, Box, Button, Grid, Typography } from'@mui/material';
+import {  Timeline, TimelineConnector, TimelineContent, TimelineDot, TimelineItem, TimelineSeparator } from '@mui/lab';
+import { Routes, Route, Link } from "react-router-dom";
 
 const theme = createTheme({
   palette: {
@@ -14,10 +16,10 @@ const theme = createTheme({
     MuiTypography: {
       defaultProps: {
         variantMapping: {
-          title: 'h1',
-          subtitle: 'h2',
-          body1: 'span',
-          body2: 'span',
+          heading: 'h1',
+          subheading: 'h2',
+          short_text: 'h3',
+          body: 'span',
         },
       },
     },
@@ -48,7 +50,11 @@ function App() {
             <Navigation />
           </Grid>
           <Grid item xs={12}>
-            <Body />
+            <Routes>
+              <Route path="/"  element={<Home />} />
+              <Route path="/skills" element={<Skills />} />
+              <Route path="/experience" element={<Experience />} />
+            </Routes>
           </Grid>
         </Grid>
       </ThemeProvider>
@@ -72,36 +78,95 @@ function Navigation() {
         sx={{padding:'1em'}}
       >
         <Grid item>
-          <StyledButton fullWidth>About Me</StyledButton>
+          <StyledButton fullWidth>
+            <Link to='/'>
+              About Me
+            </Link>
+          </StyledButton>
         </Grid>
         <Grid item>
-          <StyledButton fullWidth>Experience</StyledButton>
+          <StyledButton fullWidth>
+            <Link to='/experience'>
+              Experience
+            </Link>
+          </StyledButton>
         </Grid>
         <Grid item>
-          <StyledButton fullWidth>Skills</StyledButton>
+          <StyledButton fullWidth>
+            <Link to='/skills'>
+              Skills
+            </Link>
+          </StyledButton>
         </Grid>
       </Grid>
     </AppBar>
   )
 }
 
-function Body() {
+function Home() {
   return(
     <Box sx={{
         paddingTop:'1em',
         paddingBottom:'2em',
         marginBottom:'2em'}}>
-      <Typography variant='title'>
+      <Typography variant='heading'>
         Hi there! 👋🏻
       </Typography>
-      <Typography variant='h6'>
-        {`I'm`} <Box component='span' fontWeight='fontWeightBold'>Janella Co</Box>!
+      <Typography variant='subheading'>
+        {`I'm`} <Box component='span' fontWeight='fontWeightStrong'>Janella Co</Box>!
       </Typography>
-      <Typography variant='body1'>
-        A <strong>full-stack software engineer</strong> based in Lutz, FL. I am currently looking for <strong>associate-level</strong> software engineering or web developer opportunities! 
+      <Typography variant='short_text'>
+        A <Box component='span' fontWeight='fontWeightBold'>full-stack software engineer</Box> based in Lutz, FL. I am currently looking for <Box component='span' fontWeight='fontWeightStrong'>associate-level</Box> software engineering or web developer opportunities! 
+      </Typography>
+      <Typography variant='body'>
+        I am originally from the Philippines and moved to the United States in 2018. I studied at De La Salle Unviversity in 
+        Manila for a bit, before transferring to Hillsborough Community College (HCC) for my Associates degree.
+        After completing my Associates, I transferred to University of South Florida (USF) for my {`Bachelor's`} degree in Computer Science.
+        I graduated from USF in May 2020.         
+      </Typography>
+      <Typography variant='body'>
+        My first exposure to web development was in sixth grade–where we were taught HTML and CSS. Since then 
       </Typography>
     </Box>
   )
 }
 
+function Skills() {
+  return (
+    <>
+      <Grid>
+        <p>My skills will go here!</p>
+      </Grid>
+    </>
+  )
+}
+
+function Experience() {
+  return (
+    <>
+      <Grid>
+        <Grid item>
+          <Typography variant='h4'>Experience</Typography>
+        </Grid>
+        <Grid item>
+          <Timeline position='alternate'>
+            <TimelineItem>
+              <TimelineSeparator>
+                <TimelineDot />
+                <TimelineConnector />
+              </TimelineSeparator>
+              <TimelineContent>GitHub</TimelineContent>
+            </TimelineItem>
+            <TimelineItem>
+              <TimelineSeparator>
+                <TimelineDot />
+              </TimelineSeparator>
+              <TimelineContent>Bashpole Software, Inc.</TimelineContent>
+            </TimelineItem>
+          </Timeline>
+        </Grid>
+      </Grid>
+    </>
+  )
+}
 export default App
